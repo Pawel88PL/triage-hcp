9ccb44044e65e21e3edf92db8eef30f9c281dccf
Pawel Staniul <pstaniul@gmail.com>
2023-03-21 16:30:46 +0100

    Dodano pliki "AccountController.cs", "Credential.cs", stronę do logowania "Login.cshtml"

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using triage_hcp.Models;
+
+namespace triage_hcp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/Credential.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace triage_hcp.Models
+{
+    public class Credential
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
