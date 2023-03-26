e377d8ddb0602e2bbafc46a40cd084f9332f39a8
Pawel Staniul <pstaniul@gmail.com>
2023-03-26 15:14:50 +0200

    Utworzono stronę dla pacjenta z zakończoną obsługą. Poprawiono stronę edycji danych o pacjencie.

--- Controllers/EditController.cs
+++ Controllers/EditController.cs
@@ -19,55 +19,8 @@ namespace triage_hcp.Controllers
             _context = context;
         }
 
-        // GET: Edit
-        public async Task<IActionResult> Index()
-        {
-              return _context.Pacjenci != null ? 
-                          View(await _context.Pacjenci.ToListAsync()) :
-                          Problem("Entity set 'DbTriageContext.Pacjenci'  is null.");
-        }
-
-        // GET: Edit/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Pacjenci == null)
-            {
-                return NotFound();
-            }
-
-            var pacjent = await _context.Pacjenci
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (pacjent == null)
-            {
-                return NotFound();
-            }
-
-            return View(pacjent);
-        }
-
-        // GET: Edit/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Edit/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Surname,Pesel,Age,Gender,Room,Diagnosis,Color,DateTime,Doctor,Active,Epikryza,ObserwacjeRatPiel,CoDalejZPacjentem")] Pacjent pacjent)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(pacjent);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(pacjent);
-        }
-
-        // GET: Edit/Edit/5
+        
+        
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Pacjenci == null)
@@ -83,9 +36,7 @@ namespace triage_hcp.Controllers
             return View(pacjent);
         }
 
-        // POST: Edit/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Surname,Pesel,Age,Gender,Room,Diagnosis,Color,DateTime,Doctor,Active,Epikryza,ObserwacjeRatPiel,CoDalejZPacjentem")] Pacjent pacjent)
@@ -118,42 +69,58 @@ namespace triage_hcp.Controllers
             return View(pacjent);
         }
 
-        // GET: Edit/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+
+
+
+        public async Task<IActionResult> Done(int? id)
         {
             if (id == null || _context.Pacjenci == null)
             {
                 return NotFound();
             }
 
-            var pacjent = await _context.Pacjenci
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var pacjent = await _context.Pacjenci.FindAsync(id);
             if (pacjent == null)
             {
                 return NotFound();
             }
-
             return View(pacjent);
         }
 
-        // POST: Edit/Delete/5
-        [HttpPost, ActionName("Delete")]
+
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> Done(int id, [Bind("Id,Name,Surname,Pesel,Age,Gender,Room,Diagnosis,Color,DateTime,Doctor,Active,Epikryza,ObserwacjeRatPiel,CoDalejZPacjentem")] Pacjent pacjent)
         {
-            if (_context.Pacjenci == null)
+            if (id != pacjent.Id)
             {
-                return Problem("Entity set 'DbTriageContext.Pacjenci'  is null.");
+                return NotFound();
             }
-            var pacjent = await _context.Pacjenci.FindAsync(id);
-            if (pacjent != null)
+
+            if (ModelState.IsValid)
             {
-                _context.Pacjenci.Remove(pacjent);
+                try
+                {
+                    _context.Update(pacjent);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PacjentExists(pacjent.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("List", "Pacjent");
             }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return View(pacjent);
         }
+
+
 
         private bool PacjentExists(int id)
         {
